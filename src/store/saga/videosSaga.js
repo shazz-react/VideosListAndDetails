ec711c55de296d4dd17cda36fc643a0c02a3ff3f
Shahabaz Khan <shahbaz.9676@gmail.com>
2021-05-14 10:44:59 +0530

    Adding Axios.

--- src/store/saga/videosSaga.js
+++ src/store/saga/videosSaga.js
@@ -1,10 +1,11 @@
 import * as Actions from "../actions/videosAction";
 import { put, takeLatest, all } from "redux-saga/effects";
+import axios from "axios";
 
 function* getVideosSaga() {
-  const result = yield fetch("https://example-data.draftbit.com/videos").then(
-    (response) => response.json()
-  );
+  const result = yield axios
+    .get("https://example-data.draftbit.com/videos")
+    .then((response) => response.data);
   const filtered = result.filter((item) => item.title != undefined).reverse();
   yield put({ type: Actions.GET_VIDEOS, items: filtered });
 }
